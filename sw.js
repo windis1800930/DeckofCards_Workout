d05386dd341194e7d035781f5804a7af6b20570b
Andrea Windisch <windis180093.htlweiz.at>
2023-03-13 20:22:18 +0100

    pwa test

---
+++ sw.js
@@ -0,0 +1,21 @@
+let currentPath = self.location.href.replace("/sw.js", "");
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('my-pwa-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/script.js'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
